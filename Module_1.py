c1f3855d946f8b2cf3fa344b680873016cf44db2
vbheemanathi <vbheemanathi@gmail.com>
2020-08-02 14:25:28 -0500

    Module 1 Hands On Code added

---
+++ Module_1.py
@@ -0,0 +1,34 @@
+#Create a list containing squares of numbers from 1 to 10
+
+# list1 = list(range(11))
+# list2 = []
+#
+# for i in list1:
+#     list2.append(i**2)
+#
+# list3 = [x**2 for x in list1]
+#
+#
+# print(list1)
+# print(list2)
+# print(list3)
+
+
+# Write a Function to check if year number is a leap year.
+# def is_leap_year(x):
+#     ly = 2020
+#     if (ly-x) % 4 == 0:
+#         print ("Is Leap Year")
+#     else:
+#         print("Is not leap year")
+#
+#
+# is_leap_year(1984)
+
+# Write a Function to take an array and return another array that contains the members of first
+# array that are even.
+
+import numpy as np
+a = np.array([[1,2,3],[4,5,6]])
+
+print(a)
